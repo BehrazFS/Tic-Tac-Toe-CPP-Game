69a211aa7629faf1a105677765317b7ece26ab53
BehrazFS <behrazfs@gmail.com>
2025-12-29 00:21:04 +0330

    2

--- Colored.h
+++ Colored.h
@@ -5,25 +5,25 @@
 using namespace std;
 
 enum class Foreground : int {
-        BLACK = 30,
-        RED = 31,
-        GREEN = 32,
-        YELLOW = 33,
-        BLUE = 34,
-        MAGENTA = 35,
-        CYAN = 36,
-        WHITE = 37,
-        RESET = 39,
+    BLACK = 30,
+    RED = 31,
+    GREEN = 32,
+    YELLOW = 33,
+    BLUE = 34,
+    MAGENTA = 35,
+    CYAN = 36,
+    WHITE = 37,
+    RESET = 39,
 
-        BRIGHT_BLACK = 90,
-        BRIGHT_RED = 91,
-        BRIGHT_GREEN = 92,
-        BRIGHT_YELLOW = 93,
-        BRIGHT_BLUE = 94,
-        BRIGHT_MAGENTA = 95,
-        BRIGHT_CYAN = 96,
-        BRIGHT_WHITE = 97
-    };
+    BRIGHT_BLACK = 90,
+    BRIGHT_RED = 91,
+    BRIGHT_GREEN = 92,
+    BRIGHT_YELLOW = 93,
+    BRIGHT_BLUE = 94,
+    BRIGHT_MAGENTA = 95,
+    BRIGHT_CYAN = 96,
+    BRIGHT_WHITE = 97
+};
 
 enum class Background : int {
     BLACK = 40,
